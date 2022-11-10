7a5e5c378d18c9df6992bd315bbaafc95c9b1f3a
SashaSerh <101942102+SashaSerh@users.noreply.github.com>
2022-11-11 00:31:49 +0200

    Change task #5

--- js/task-05.js
+++ js/task-05.js
@@ -1,13 +1,12 @@
 
 const inputRef = document.querySelector("#name-input")
 const outputRef = document.querySelector("#name-output")
-inputRef.addEventListener("input", (event) => {
-  const inputValue = event.target.value;
-   outputRef.disabled = !event.target.checked;
-    outputRef.textContent = `${inputValue}`;
-  
-});
 
-// inputRef.addEventListener("change", (event) => {
-   
-//   });
+
+inputRef.addEventListener("input",()=> {
+    if (inputRef.value) {
+      outputRef.textContent = inputRef.value;
+    } else {
+      outputRef.textContent = "Anonymous";
+    }
+  });
